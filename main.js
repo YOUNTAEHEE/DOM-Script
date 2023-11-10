0fbf772fa8df846bcb41d4f7d6f5b3bcb27100af
YOUNTAEHEE <f_hyang@naver.com>
2023-11-10 12:59:42 +0900

    버튼 forEach

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ DOM(Document Object Model):Web Api
 */
 
 console.dir(btns[0]);
-
+/*
 btns[0].addEventListener('click', () => {
 	console.log('엄청중요한 작업');
 });
 btns[0].addEventListener('click', () => {
 	console.log('실수로 연결');
 });
-
+*/
 /*
 btns[0].onclick = () => {
 	console.log('button1');
@@ -22,3 +22,16 @@ btns[0].onclick = () => {
 btns[0].onclick = () => {
 	console.log('button2');
 };*/
+
+//반복돌면서 각 버튼 믈릭시 button1~3출력
+btns.forEach((btn, index) => {
+	btn.addEventListener('click', () => {
+		console.log(`button${index + 1}`);
+	});
+});
+
+btns.forEach((btn, index) => {
+	btns[index].addEventListener('click', () => {
+		console.log(`button${index + 1}`);
+	});
+});
